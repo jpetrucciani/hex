9e8c801b20654e91242c4efe431033b2047f71ff
jacobi petrucciani <j@cobi.dev>
2024-12-03 00:11:01 -0500

    fix retagged open-webui 4.0.0

--- hex/hex/k8s/open-webui.nix
+++ hex/hex/k8s/open-webui.nix
@@ -13,7 +13,7 @@ let
       _v = hex.k8s._.version chart;
       latest = v4-0-0;
       v4-0-6 = _v "4.0.6" "1z97b6fb61bki224b2az9c281i2ldfval8b92dywqf8mjsmhflml"; # 2024-12-02
-      v4-0-0 = _v "4.0.0" "16g43mwla6mlgyn6jm8iajav31rk9435xyh7kfq5qjlin7w6gmcx"; # 2024-11-20
+      v4-0-0 = _v "4.0.0" "1p68zhpvcdfqkmpqvd91mazs2qdhyjcc7xnrfjc3s67rgbw4cl42"; # 2024-11-20
       v3-6-0 = _v "3.6.0" "0qkzxcvbf38j9naxgl19m6v6nyl2308a58j5nx1yvhkikrqikf6k"; # 2024-11-07
       v3-5-1 = _v "3.5.1" "1akgy4v47a168cl6wzhk1n7dkn24bpdg7id7xigf9zyxfd7vdx7d"; # 2024-11-05
       v3-4-3 = _v "3.4.3" "0q2ylkhn9v0v8m2bscjzw26q8rv38dsblh5x7v3hzbqxi09pdj4w"; # 2024-10-27
