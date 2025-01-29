d2bfc9f0ff14baf95bf2ed712fa55aaee4007b2d
jacobi petrucciani <j@cobi.dev>
2025-01-29 17:50:32 -0500

    update langflow -> 0.1.1

--- hex/hex/k8s/helm/langflow.nix
+++ hex/hex/k8s/helm/langflow.nix
@@ -13,7 +13,8 @@ let
       };
       version = rec {
         _v = hex.k8s._.version chart;
-        latest = v0-1-0;
+        latest = v0-1-1;
+        v0-1-1 = _v "0.1.1" "0kwniaplpl0h6h9qslkv3k7c8q7dzy8mwhl9zfp16sgfa0s88ywy"; # 2025-01-24
         v0-1-0 = _v "0.1.0" "016kii40x2hdzrhphl7l8jas9ikn8ykw4y0k21z95k3cljignhys"; # 2024-07-23
       };
       chart_url = langflow_chart_url name;
@@ -29,7 +30,8 @@ let
       };
       version = rec {
         _v = hex.k8s._.version chart;
-        latest = v0-1-0;
+        latest = v0-1-1;
+        v0-1-1 = _v "0.1.1" "16mkv0ajma6j65df46p351jxzjdfykns07q4yavja9qdrcc8nbsy"; # 2025-01-24
         v0-1-0 = _v "0.1.0" "009xdwpwr9i4r7rpmkfijmiaqc2gjwy7nkg9rzgyisxg8gq8xgmg"; # 2024-07-23
       };
       chart_url = langflow_chart_url name;
