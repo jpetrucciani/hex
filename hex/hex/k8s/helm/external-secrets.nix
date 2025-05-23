0c5c028abbd128d0ada1a6ca7b2f09ba0e3e4617
jacobi petrucciani <j@cobi.dev>
2025-05-23 18:48:21 -0400

    forgot to include the actual new values

--- hex/hex/k8s/helm/external-secrets.nix
+++ hex/hex/k8s/helm/external-secrets.nix
@@ -138,7 +138,7 @@ let
         , string_data ? { }
         , _beta ? false
         , apiVersion ? if _beta then "external-secrets.io/v1beta1" else "external-secrets.io/v1"
-        }: toYAMLDoc (secret { inherit name filename env store store_kind refresh_interval secret_ref namespace extract decoding_strategy metadata_policy conversion_strategy extra_data labels string_data; });
+        }@args: toYAMLDoc (secret args);
 
       secret =
         { name
