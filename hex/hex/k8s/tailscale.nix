babcc0e9bcc68faf79cfd6d82a74626c8e07cca9
jacobi petrucciani <j@cobi.dev>
2025-06-09 20:03:39 -0400

    update tailscale tag -> 1.84.2

--- hex/hex/k8s/tailscale.nix
+++ hex/hex/k8s/tailscale.nix
@@ -18,7 +18,7 @@ let
         memory = "2Gi";
       };
       tailscale_image_base = "ghcr.io/tailscale/tailscale";
-      tailscale_image_tag = "v1.84.0";
+      tailscale_image_tag = "v1.84.2";
       busybox_image_base = "busybox";
       busybox_image_tag = "1.37.0";
       cloudsql_image_base = "gcr.io/cloudsql-docker/gce-proxy";
