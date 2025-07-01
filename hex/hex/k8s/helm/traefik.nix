cf9244d4f4f5d94c95ab781c94f8bf317ce516e8
jacobi petrucciani <j@cobi.dev>
2025-07-01 10:07:02 -0400

    update traefik chart -> 36.3.0

--- hex/hex/k8s/helm/traefik.nix
+++ hex/hex/k8s/helm/traefik.nix
@@ -10,7 +10,8 @@ let
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v36-2-0;
+      latest = v36-3-0;
+      v36-3-0 = _v "36.3.0" "03rsjd0ckq2agsj48f3pkmx50i5m8a1qrh2r5xrzz1x97029q4n3"; # 2025-07-01
       v36-2-0 = _v "36.2.0" "1m5sg2jwg3pyxv5fqfcj1kha6mqr18m28h6fzd59ngvh1id2mss2"; # 2025-06-24
       v36-1-0 = _v "36.1.0" "17ksciaqq7cz0k9v39cr665ppv0p3jf4kxm6gllzaidp753r0l1n"; # 2025-06-11
       v36-0-0 = _v "36.0.0" "1cb1ss47awhwqqcgzqyxgvzw65j6zaz0kxygvspnvr1jjvd1m97k"; # 2025-06-06
