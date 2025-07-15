673fc5144f3d748c6d7f11bbb01e158d2ed89a9b
jacobi petrucciani <j@cobi.dev>
2025-07-14 20:55:16 -0400

    update questdb -> 1.0.8

--- hex/hex/k8s/helm/questdb.nix
+++ hex/hex/k8s/helm/questdb.nix
@@ -8,7 +8,8 @@ let
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v1-0-7;
+      latest = v1-0-8;
+      v1-0-8 = _v "1.0.8" "00mln3j0hiyaans8agw8wxifw65ddy93vwp7xi71qbyc6rq9i15z"; # 2025-07-11
       v1-0-7 = _v "1.0.7" "0nvjvfkb89vld0k5mqnbcfaazlgnqyjf2x3is7g0kfnmipivskxs"; # 2025-05-30
       v1-0-5 = _v "1.0.5" "1rfkgp76f1mzwjb7y2b7z8d0g39cpyny7p2v32zifg1i7f48ivyn"; # 2025-04-28
       v0-42-1 = _v "0.42.1" "1p718y7fg85j59f6mr3jnv4izhb7xr290ijd0zpfbqqx3hssjiix"; # 2025-02-05
