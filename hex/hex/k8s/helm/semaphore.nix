9bd181e7006600c95742c1e66e504d22ddef817b
jacobi petrucciani <j@cobi.dev>
2025-09-17 08:10:16 -0400

    update semaphore -> 16.0.4

--- hex/hex/k8s/helm/semaphore.nix
+++ hex/hex/k8s/helm/semaphore.nix
@@ -7,7 +7,9 @@ let
     };
     version = rec {
       _v = hex.k8s._.version chart;
-      latest = v16-0-2;
+      latest = v16-0-4;
+      v16-0-4 = _v "16.0.4" "0g66cljpv6qzn8xmh4m14alb3g7qlysd1g7jrsbfgh1i8lby7pmr"; # 2025-09-17
+      v16-0-3 = _v "16.0.3" "1bjg0lgx6l3rxrs08k5h9ws28az4iqc0wlysyzkk3s85fp8l6r35"; # 2025-09-16
       v16-0-2 = _v "16.0.2" "0l3k95a1im2v5lnasq25n37y5h2b5vzyp2yvzyw7sqn0gikqai39"; # 2025-07-24
       v16-0-1 = _v "16.0.1" "13ll37ipqmh35qg9346j9zsc1lbf8cv4xmih4cy51zykywc07dnx"; # 2025-07-21
       v16-0-0 = _v "16.0.0" "0hc8mbb3cnk1yqr5hbyzkdniv35barkpxh1d9m64pb5vb125af6y"; # 2025-06-14
