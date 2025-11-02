13f0c7ebfead27d460144d974637c1fa8ff218d0
jacobi petrucciani <j@cobi.dev>
2025-11-02 11:10:16 -0500

    update litellm -> 1.79.0

--- hex/hex/k8s/svc/litellm.nix
+++ hex/hex/k8s/svc/litellm.nix
@@ -2,7 +2,7 @@
 let
   litellm =
     { name ? "litellm"
-    , version ? "v1.78.5"
+    , version ? "v1.79.0"
     , namespace ? "default"
     , image_registry ? "ghcr.io/berriai"
     , image_base ? "litellm-database"
