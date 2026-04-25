ccb110f33ed60834a81c4a859d1eee4ed10c3b89
jacobi petrucciani <j@cobi.dev>
2026-04-25 14:53:53 -0400

    update flakes, use better system

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { _compat ? import ./flake-compat.nix
 , pkgs ? import _compat.inputs.nixpkgs { overlays = [ pog.overlay ]; }  # TODO: document this, or fix it so we don't have to rely on the end user to provide pog in their pkgs
 , pog ? import _compat.inputs.pog { }
-, system ? pkgs.system
+, system ? pkgs.stdenv.hostPlatform.system
 }:
 let
   params = { inherit system pkgs; };
