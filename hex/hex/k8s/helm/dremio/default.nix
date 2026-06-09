0a02a8b5e405a8b2b347f1ed20f07317258b3284
jacobi petrucciani <j@cobi.dev>
2026-06-09 19:00:44 -0400

    found the values

--- hex/hex/k8s/helm/dremio/default.nix
+++ hex/hex/k8s/helm/dremio/default.nix
@@ -6,7 +6,7 @@ let
       name = "dremio";
       namespace = "default";
     };
-    values_url = "https://github.com/godremio/helm/blob/main/charts/dremio/values.yaml";
+    values_url = "https://github.com/dremio/dremio-cloud-tools/blob/master/charts/dremio_v2/values.yaml";
     version = hex.k8s._.versionMap { inherit chart; versionFile = ./dremio.json; };
     chart_url = version: "oci://quay.io/dremio/dremio-helm:${version}";
     chart = hex.k8s._.chart { inherit defaults chart_url; };
